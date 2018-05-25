6b031cfd63268c8a2addb8cdec464ff9effe4f16
pglebow <pglebow@gmail.com>
2018-05-25 16:21:36 -0700

    Setup

---
+++ src/main/java/com/glebow/bnf/BeamNucleotideFrequencyApplication.java
@@ -0,0 +1,12 @@
+package com.glebow.bnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamNucleotideFrequencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamNucleotideFrequencyApplication.class, args);
+	}
+}
